b8ee4b0457ae68b2f9f2e016fd23c8c8d14d0749
Owen Tarswell <owentarswell@hotmail.com>
2018-01-09 15:42:23 -0500

    Initial commit

---
+++ IWordGame.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WordGame
+{
+    
+    [ServiceContract]
+    public interface IWordGame
+    {
+        // Returns true if the game is already being hosted or false otherwise
+        [OperationContract]
+        bool isGameBeingHosted();
+
+        // User 'userName' tries to host the game with word 'wordToScramble'
+        // The function returns the name of the person hosting the game
+        // Exception: game is already being hosted by someone else
+        [FaultContract(typeof(GameBeingHostedException))]
+        [OperationContract]
+        string hostGame(String userName, String wordToScramble);
+
+        // Player 'playerName' tries to join the game
+        // The function returns a Word object containing the host's (un)scrambled words
+        // Exception: maximum number of players reached
+        // Exception: host cannot join the game
+        // Exception: nobody is hosting the game
+        [FaultContract(typeof(MaximunNumberOfPlayersReachedException))]
+        [FaultContract(typeof(HostCannotJoinTheGameException))]
+        [FaultContract(typeof(GameIsNotBeingHostedException))]
+        [OperationContract]
+        Word join(string playerName);
+
+        // Player 'playerName' guesses word 'guessedWord' compared with word 'unscrambledWord'
+        // Returns true if 'guessedWord' is identical to 'unscrambledWord' or false otherwise
+        // Exception: user is not playing the game
+        [FaultContract(typeof(PlayerNotPlayingTheGameException))]
+        [OperationContract]
+        bool guessWord(string playerName, string guessedWord, string unscrambledWord);
+
+    }
+
+    [DataContract]
+    public class Word
+    {
+        [DataMember]
+        public string unscrambledWord; // word typed by the game's host
+        [DataMember]
+        public string scrambledWord;
+    }
+
+    [DataContract]
+    public class GameBeingHostedException
+    {
+        [DataMember]
+        public string reason;
+    }
+    [DataContract]
+    public class MaximunNumberOfPlayersReachedException
+    {
+        [DataMember]
+        public int playerNum;
+        [DataMember]
+        public string reason;
+    }
+    [DataContract]
+    public class HostCannotJoinTheGameException
+    {
+        [DataMember] public string playerName;
+        [DataMember] public string reason;
+    }
+    [DataContract]
+    public class GameIsNotBeingHostedException
+    {
+        [DataMember] public string reason;
+    }
+    [DataContract]
+    public class PlayerNotPlayingTheGameException
+    {
+        [DataMember] public string playerName;
+        [DataMember] public string reason;
+    }
+
+}
---
+++ WordGame.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WordGame
+{
+    [ServiceBehavior]
+    public class WordGame : IWordGame
+    {
+        // the maximum number of players allowed playing simultaneously
+        private const int MAX_PLAYERS = 5;
+        // the user hosting the game. If it's null nobody is hosting the game
+        private static String userHostingTheGame = null;
+        // the Word object that contains the scrambled and unscrambled words
+        private static Word gameWords;
+        // the list of players playing the game
+        private static List<string> activePlayers = new List<string>();
+
+        [OperationBehavior]
+        public bool isGameBeingHosted()
+        {
+            if (userHostingTheGame == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        [OperationBehavior]
+        public string hostGame(string playerName, string wordToScramble)
+        {
+
+            if (userHostingTheGame != null)
+            {
+                GameBeingHostedException exception = new GameBeingHostedException();
+                exception.reason = "Game already being hosted";
+                throw new FaultException<GameBeingHostedException>(exception);
+
+            }
+            userHostingTheGame = playerName;
+            gameWords = new Word();
+            gameWords.unscrambledWord = wordToScramble;
+            gameWords.scrambledWord = scrambleWord(wordToScramble);
+
+            return gameWords.scrambledWord;
+
+        }
+
+        [OperationBehavior]
+        public Word join(string playerName)
+        {
+            activePlayers.Add(playerName);
+            if (activePlayers.Count > MAX_PLAYERS)
+            {
+                MaximunNumberOfPlayersReachedException exception = new MaximunNumberOfPlayersReachedException();
+                exception.playerNum = MAX_PLAYERS;
+                exception.reason = "Too many players in game!";
+                throw new FaultException<MaximunNumberOfPlayersReachedException>(exception);
+            }
+            if (playerName == userHostingTheGame)
+            {
+                HostCannotJoinTheGameException exception = new HostCannotJoinTheGameException();
+                exception.playerName = playerName;
+                exception.reason = "Host cannot join game!";
+                throw new FaultException<HostCannotJoinTheGameException>(exception);
+            }
+            if (!isGameBeingHosted())
+            {
+                GameIsNotBeingHostedException exception = new GameIsNotBeingHostedException();
+                exception.reason = "Game is no longer being hosted!";
+                throw new FaultException<GameIsNotBeingHostedException>(exception);
+            }
+            return gameWords;
+        }
+
+        [OperationBehavior]
+        public bool guessWord(string playerName, string guessedWord, string unscrambledWord)
+        {
+
+            if (!activePlayers.Contains(playerName))
+            {
+                PlayerNotPlayingTheGameException exception = new PlayerNotPlayingTheGameException();
+                exception.playerName = playerName;
+                exception.reason = "Player is not in current game.";
+                throw new FaultException<PlayerNotPlayingTheGameException>(exception);
+            }
+            if (guessedWord == unscrambledWord)
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+
+        private string scrambleWord(string word)
+        {
+            char[] chars = word.ToArray();
+            Random r = new Random(2011);
+            for (int i = 0; i < chars.Length; i++)
+            {
+                int randomIndex = r.Next(0, chars.Length);
+                char temp = chars[randomIndex];
+                chars[randomIndex] = chars[i];
+                chars[i] = temp;
+            }
+            return new string(chars);
+        }
+    }
+}
